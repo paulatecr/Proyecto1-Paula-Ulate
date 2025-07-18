60a9fefef6a53ae87ae9c5eb85faeba137083a6d
Paula Ulate <paulatecr@gmail.com>
2025-07-17 20:09:25 -0600

    Jueves

--- Models/Entrega.cs
+++ Models/Entrega.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,17 +7,12 @@ namespace Proyecto1_Paula_Ulate.Models
 {
     public class Entrega
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public DateTime FechaEntrega { get; set; }
 
-        [Required]
         public string EntregadoPor { get; set; }
 
         // Relación con Solicitud
-        [ForeignKey("Solicitud")]
         public int SolicitudId { get; set; }
         public Solicitud Solicitud { get; set; }
 
--- Models/Repuesto.cs
+++ Models/Repuesto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,23 +7,10 @@ namespace Proyecto1_Paula_Ulate.Models
 {
     public class Repuesto
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public string Nombre { get; set; }
 
         public string Descripcion { get; set; }
-
-        [Required]
         public int Cantidad { get; set; }
-
-        [Required]
-        public string Ubicacion { get; set; }
-
-        [Required]
-        public decimal Precio { get; set; }
-
-        public DateTime FechaIngreso { get; set; }
     }
 }
