a076725b5363fd8c9526ffe36e623ab8bc7f2613
Paula Ulate <paulatecr@gmail.com>
2025-07-19 08:13:18 -0600

    Controllers Start

---
+++ Controllers/RepuestoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_Paula_Ulate.Controllers
+{
+    public class RepuestoController : Controller
+    {
+        // GET: Repuesto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
